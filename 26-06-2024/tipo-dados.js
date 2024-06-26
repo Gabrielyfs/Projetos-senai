18fbb35747dee793827292ed4b25b0f7f9d00f04
Gabrielyfs <felixdesouzagabriely@gmail.com>
2024-06-26 09:47:04 -0300

    adicionando variaveis

---
+++ 26-06-2024/tipo-dados.js
@@ -0,0 +1,11 @@
+let idadeAluno = 18; //inteiro
+let valorProduto = 5.58; //numeros flutuantes com casas decimais
+let nomeAluno = "Pedro Henrique"; //variavel de texto String
+let casado = true; //bolleano
+let linguagens = ["Java", "C#", "Phyton"]; //array: conjunto de elementos
+
+console.log("Idade do Aluno: " + idadeAluno);
+console.log("R$ " + valorProduto);
+console.log("Nome do Aluno: " + nomeAluno);
+console.log("Casado: " + casado);
+console.table(linguagens[2]);
---
+++ 26-06-2024/variavel.js
@@ -0,0 +1,9 @@
+let mensagem = "Olá mundo";
+let nome; //cria uma variavel indefinida
+let disciplina = null; //cria uma variável vazia
+let texto =  "Exemplo"; //cria uma variável com um valor
+
+//console.log(mensagem);
+console.log(nome);
+console.log(disciplina);
+console.log(texto);
