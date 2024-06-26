efd48cf3d32bb4be3046688af7425a73ab3a66a3
Gabrielyfs <felixdesouzagabriely@gmail.com>
2024-06-26 10:33:29 -0300

    Descobrindo funções no js

--- 26-06-2024/variavel.js
+++ 26-06-2024/variavel.js
@@ -3,7 +3,24 @@ let nome; //cria uma variavel indefinida
 let disciplina = null; //cria uma variável vazia
 let texto =  "Exemplo"; //cria uma variável com um valor
 
-//console.log(mensagem);
+console.log(mensagem);
 console.log(nome);
 console.log(disciplina);
 console.log(texto);
+
+console.log(" ");
+
+const tom = "Tom";
+const firstTom = "Hanks";
+const secondTom = "Cruise";
+const myFavoriteTom = "My Favorite Tom: " + tom + " " + firstTom;
+
+console.log(myFavoriteTom)
+
+console.log(" ");
+
+// divisão
+
+const programmingLanguage = "JavaScript";
+const js = programmingLanguage[0] + programmingLanguage[4];
+console.log(js);
