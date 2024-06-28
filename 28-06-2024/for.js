a8d23cc5bb57eb58640b87afc10883e0e5fc593f
Gabrielyfs <felixdesouzagabriely@gmail.com>
2024-06-28 11:43:37 -0300

    Fazendo listas

--- 28-06-2024/for.js
+++ 28-06-2024/for.js
@@ -8,3 +8,11 @@ console.log("");
 for (let x = 1; x <= 10; x++) {
     console.log(`Esta é a ${x}ª vez que esta frase é exibida no console.`)
 }
+
+console.log("");
+
+let frutas = ["maça","banana", "pera"];
+
+for (let v = 0; v < frutas.length; v++) {
+    console.log(`- ${frutas[v]}`);
+}
