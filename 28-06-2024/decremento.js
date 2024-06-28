73667e9aac5a1a1cace9b0108cd24f47cbadcd49
Gabrielyfs <felixdesouzagabriely@gmail.com>
2024-06-28 11:36:10 -0300

    Utilizando o for

---
+++ 28-06-2024/decremento.js
@@ -0,0 +1,16 @@
+let valor = 7;
+console.log(valor);//valendo 7
+
+valor--;
+console.log(valor);//valendo 6
+
+valor-= 4;
+console.log(valor);//valendo 2
+
+//lógica
+
+valor *= 4;
+console.log(valor);//multiplicação
+
+valor /= 8;
+console.log(valor);//divisão
---
+++ 28-06-2024/for.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <= 3; i++) {
+    console.log(`Agora i vale: ${i}`);
+}
+
+console.log("");
+
+//desafio
+for (let x = 1; x <= 10; x++) {
+    console.log(`Esta é a ${x}ª vez que esta frase é exibida no console.`)
+}
