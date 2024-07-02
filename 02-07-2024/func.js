46c6731f9eb4d0f53d62b028028ebc6cafb0a643
Gabrielyfs <felixdesouzagabriely@gmail.com>
2024-07-02 09:50:05 -0300

    criando funções

---
+++ 02-07-2024/func.js
@@ -0,0 +1,10 @@
+function nomeFuncao() {
+    console.log("Esta é uma função")
+}
+nomeFuncao();
+
+function calc(arg1, arg2) {
+    return arg1 + arg2;
+}
+console.log(calc(4, 4));
+console.log(calc(5, 5));
