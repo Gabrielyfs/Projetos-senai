a08fd2c648c1c00fa990369cff47b01e9e0f96c6
Gabrielyfs <felixdesouzagabriely@gmail.com>
2024-07-04 11:27:22 -0300

    Criando cadastro com 'print' na tela

---
+++ 04-07-2024/Cadastro/cadastro.js
@@ -0,0 +1,19 @@
+//obtém os valores dos campos de entrada
+let form = document.querySelector("#cadastroForm");
+let cadastroList = document.querySelector("#cadastroList");
+let nome = document.querySelector("#nome");
+let email = document.querySelector("#email");
+let cidade = document.querySelector("#cidade")
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let listItem = document.createElement("li")
+    listItem.innerHTML = `<strong>Nome:</strong> ${nome.value} <br><strong>Email:</strong> ${email.value}<br><strong>Cidade:</strong> ${cidade.value}`
+
+    //adiciona no item na lista
+    cadastroList.appendChild(listItem)
+
+    //limpa o input
+    form.reset();
+})
